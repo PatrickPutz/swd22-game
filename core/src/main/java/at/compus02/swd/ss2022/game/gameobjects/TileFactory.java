1478d98bae73e34a984b55b220ca06d7476f0db4
patrick-putz <Putz.Patrick.1996@gmail.com>
2022-05-15 13:21:41 +0200

    TileFactory - createTile, createTileBackgroundArea

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/TileFactory.java
@@ -0,0 +1,48 @@
+package at.compus02.swd.ss2022.game.gameobjects;
+
+import at.compus02.swd.ss2022.game.gameobjects.tiles.*;
+
+import java.util.ArrayList;
+
+public class TileFactory {
+
+    private Tile getTileType(TileType tileType){
+        switch (tileType) {
+            case GRASS:
+                return new TileGrass();
+            case GRAVEL:
+                return new TileGravel();
+            case WALL:
+                return new TileWall();
+            case WATER:
+                return new TileWater();
+            default:
+                return null;
+        }
+    }
+
+    public Tile createTile(TileType tileType, int positionX, int positionY){
+        Tile tile = getTileType(tileType);
+        tile.setPosition(positionX, positionY);
+        return tile;
+    }
+
+    public ArrayList<Tile> createTileBackgroundArea(TileType tileType, int topLeftX, int topLeftY, int numberOfTilesHorizontal, int numberOfTilesVertical){
+        ArrayList<Tile> tiles = new ArrayList<>();
+        int currentX = topLeftX;
+        int currentY = topLeftY;
+
+        for(int i = 0; i < numberOfTilesVertical; i++){
+            for(int j = 0; j < numberOfTilesHorizontal; j++) {
+                Tile tile = createTile(tileType, currentX, currentY);
+                currentX += tile.getWidth();
+                tiles.add(tile);
+            }
+            currentX = topLeftX;
+            currentY -= tiles.get(0).getHeight();
+        }
+
+        return tiles;
+    }
+
+}
